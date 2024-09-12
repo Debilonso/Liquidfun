6ddd1452f107224392c5db29f333dd231e001303
Debilonso <149551019+Debilonso@users.noreply.github.com>
2024-09-12 10:00:04 -0500

    Update CasoUno.h

--- CasoUno.h
+++ CasoUno.h
@@ -5,11 +5,10 @@
 class ProyectoCasoUno : public Test
 {
 public:
-	//b2PolygonShape& DestroyBox = b2PolygonShape();
 	b2PolygonShape DestroyBox;
 	b2Transform transformDestruction;
 	b2ParticleSystem* m_particleSystems[100];
-	int NUM_PARTICLE_SYSTEMS = 100;
+	int NUM_PARTICLES = 100;
 
 	ProyectoCasoUno()
 	{
@@ -44,53 +43,59 @@ public:
 		b2PolygonShape leftBox;
 		leftBox.SetAsBox(10.0f, 120.0f);
 		leftBody->CreateFixture(&leftBox, 0.0f);
-		//creando ela pared de la izquierda de la caja.
+		//creando la pared de la izquierda de la caja.
 		const b2ParticleSystemDef particleSystemDef2;
-		////...................................................
-		//b2BodyDef DestroyBodyDef;
-		//DestroyBodyDef.position.Set(-50.0f, 90.0f);
-		//b2Body* DestroyBody = m_world->CreateBody(&DestroyBodyDef);
-		DestroyBox.SetAsBox(150.0f, 70.0f);
-		//b2Fixture* fixtureDestroy=DestroyBody->CreateFixture(&DestroyBox, 0.0f);
-		//fixtureDestroy->SetSensor(true);
-		b2Transform transformDestruction;
-		b2Vec2 posicionDestroy(-50.0f, 90.0f);
-		transformDestruction.Set(posicionDestroy,0);
-		//Se crea una figura para destruir las particulas.
-		for (int i = 0; i < NUM_PARTICLE_SYSTEMS; ++i) {
+		for (int i = 0; i < NUM_PARTICLES; ++i) {
 			m_particleSystems[i] = m_world->CreateParticleSystem(&particleSystemDef2);
 			m_particleSystems[i]->SetMaxParticleCount(1000);
 			b2ParticleDef particula_i;
 			particula_i.flags = b2_elasticParticle;
 			particula_i.color.Set(0, 0, 255, 255);
 			particula_i.position.Set(i, 0);
-			int tempIndex = m_particleSystem->CreateParticle(particula_i);
+			int tempIndex = m_particleSystems[i]->CreateParticle(particula_i);
+			b2Vec2* velocidad =m_particleSystems[i]->GetVelocityBuffer();
 			m_particleSystems[i]->SetRadius(0.25f);
 			m_particleSystems[i]->SetMaxParticleCount(1000);
-			m_particleSystems[i]->DestroyParticlesInShape(DestroyBox, transformDestruction);
 		}
 	}
 
 	virtual void Step(Settings* settings)
 	{
 		Test::Step(settings);
-		printf("entro\n");
-		b2Vec2 pos(20.0f, 150.0f);
+		/*
 		for (int i = 0; i < NUM_PARTICLE_SYSTEMS; ++i) {
+			m_particleSystems[i]->DestroyParticlesInShape(DestroyBox, transformDestruction);
+		}
+		*/
+		DestroyBox = b2PolygonShape();
+		DestroyBox.SetAsBox(30.0f, 30.0f);
+		b2Transform transformDestruction;
+		b2Vec2 posicionDestroy = b2Vec2(50.0f, -10.0f);
+		transformDestruction.Set(posicionDestroy, 0);
+		//printf("entro\n");
+		b2Vec2 pos(20.0f, 150.0f);
+		for (int i = 0; i < NUM_PARTICLES; ++i) {
 			b2Vec2* posicionesParticulas = m_particleSystems[i]->GetPositionBuffer();
 			for (int j = 0; j < 1; j++) {
-				//if (DestroyBox.TestPoint(transformDestruction, posicionesParticulas[j]))
-				float dist = (pos - posicionesParticulas[j]).Length();
-				printf("dist %f\n", dist);
-				if(dist < 10000)
+				//printf("Posicion %f %f\n", posicionesParticulas[j].x, posicionesParticulas[j].y);
+				if (DestroyBox.TestPoint(transformDestruction, posicionesParticulas[j]))
 				{
 					m_particleSystems[i]->DestroyParticle(j);
-					printf("destruyo\n");
+					//printf("destruyo\n");
 				}
 			}
 		}
+		//Toda esta parte es la función para destruir las particulas que toquen un box especifico.
+		//por eso se debe hacer en el step que es la función que llama por cada paso.
+		
 	}
 
+	void ColorVel(b2ParticleColor Colors,b2Vec2 velocidad) {
+		//función para cambiar el color de las particulas segun la velovidad.
+		double velx = velocidad.x;
+		double vecy = velocidad.y;
+		Colors.Set(velx, vecy, 2* vecy, 2/ velx);
+	}
 	static Test* Create()
 	{
 		return new ProyectoCasoUno;

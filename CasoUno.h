e12452d6db8339976d2e9886e8e9dc1726372fb7
Debilonso <149551019+Debilonso@users.noreply.github.com>
2024-09-17 18:05:29 -0500

    Update CasoUno.h
    Colores según amplitud.

--- CasoUno.h
+++ CasoUno.h
@@ -2,100 +2,148 @@
 #ifndef _CASO_UNO_H
 #define _CASO_UNO_H
 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <valarray>
+#include <vector>
+
+const int N = 100;
+const int pasos = 100;
+
+double radio = 0.25;
+double pointsY = 0.0;
+double pointsX = 0.0;
+
 class ProyectoCasoUno : public Test
 {
 public:
+	int Tiempo = 0;
 	b2PolygonShape DestroyBox;
 	b2Transform transformDestruction;
-	b2ParticleSystem* m_particleSystems[100];
-	int NUM_PARTICLES = 100;
+	const b2ParticleSystemDef particleSystemDef;
+	b2ParticleSystem* particleSystem = m_world->CreateParticleSystem(&particleSystemDef);
+	// construimos la rejilla,
+	const double dx = 0.01;
+	const double dy = 0.01;
+	const double dt = 0.005;
+	const double c = 1.0;   //velocidad de propagacion de la onda
+	//construir la barrera, 
+	const int barrera_x = 50;
+	const int orificio_y1 = 45;
+	const int orificio_y2 = 55;
+	const double barrera_amplitud = 0.0;  //por las condiciones de la barrera queremos que al chocar el valor de la amplitud se haga cero
+	//implementacion del metodo de diferncias finitas
+	double u_present[N][N];
+	double u_past[N][N];
+	double u_future[N][N];
+
+	double Resultados[N][N][pasos];
 
 	ProyectoCasoUno()
 	{
-		b2Vec2 gravity(0.0f, -9.81f);
+		b2Vec2 gravity(0.0f, 0.0f);
 		m_world->SetGravity(gravity);
-		//se pone la gravedad.
-		b2BodyDef floorBodyDef;
-		floorBodyDef.position.Set(0.0f, -10.0f);
-		b2Body* floorBody = m_world->CreateBody(&floorBodyDef);
-		b2PolygonShape floorBox;
-		floorBox.SetAsBox(150.0f, 10.0f);
-		floorBody->CreateFixture(&floorBox, 0.0f);
-		//creando el suelo.
-		b2BodyDef rigthWallBodyDef;
-		rigthWallBodyDef.position.Set(160.0f, 85.0f);
-		b2Body* rigthWallBody = m_world->CreateBody(&rigthWallBodyDef);
-		b2PolygonShape rigthWallBox;
-		rigthWallBox.SetAsBox(10.0f, 105.0f);
-		rigthWallBody->CreateFixture(&rigthWallBox, 0.0f);
-		//se crea la pared de la derecha.
-		b2BodyDef roofBodyDef;
-		roofBodyDef.position.Set(10.0f, 200.0f);
-		b2Body* roofBody = m_world->CreateBody(&roofBodyDef);
-		b2PolygonShape roofBox;
-		roofBox.SetAsBox(160.0f, 10.0f);
-		roofBody->CreateFixture(&roofBox, 0.0f);
-		//se crea el techo de la caja.
-		//creando el suelo
-		b2BodyDef leftBodyDef;
-		leftBodyDef.position.Set(-160.0f, 90.0f);
-		b2Body* leftBody = m_world->CreateBody(&leftBodyDef);
-		b2PolygonShape leftBox;
-		leftBox.SetAsBox(10.0f, 120.0f);
-		leftBody->CreateFixture(&leftBox, 0.0f);
-		//creando la pared de la izquierda de la caja.
-		const b2ParticleSystemDef particleSystemDef2;
-		for (int i = 0; i < NUM_PARTICLES; ++i) {
-			m_particleSystems[i] = m_world->CreateParticleSystem(&particleSystemDef2);
-			m_particleSystems[i]->SetMaxParticleCount(1000);
-			b2ParticleDef particula_i;
-			particula_i.flags = b2_elasticParticle;
-			particula_i.color.Set(0, 0, 255, 255);
-			particula_i.position.Set(i, 0);
-			int tempIndex = m_particleSystems[i]->CreateParticle(particula_i);
-			b2Vec2* velocidad =m_particleSystems[i]->GetVelocityBuffer();
-			m_particleSystems[i]->SetRadius(0.25f);
-			m_particleSystems[i]->SetMaxParticleCount(1000);
-		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				b2ParticleDef particula_i;
+				particula_i.flags = b2_elasticParticle;
+				particula_i.color.Set(0, 0, 255, 255);
+				particula_i.position.Set(i/2, j/2);
+				int tempIndex = particleSystem->CreateParticle(particula_i);
+				b2Vec2* velocidad = particleSystem->GetVelocityBuffer();
+				particleSystem->SetRadius(radio*0.5);
+				particleSystem->SetMaxParticleCount(1000000000);
+			}
+		}	
+		amplitud();
 	}
 
 	virtual void Step(Settings* settings)
 	{
 		Test::Step(settings);
-		/*
-		for (int i = 0; i < NUM_PARTICLE_SYSTEMS; ++i) {
-			m_particleSystems[i]->DestroyParticlesInShape(DestroyBox, transformDestruction);
+
+		if (Tiempo >= 0)
+		{ 
+			b2ParticleColor* ColoresArreglo = particleSystem->GetColorBuffer();
+			for (int i = 1; i < N-1; i++) {
+				for (int j = 1; j < N-1; j++) {
+					//TODO revisar si tiene bien el id.
+					int id = (i* N + j);
+					ColoresArreglo[id] = ColorAmp(i, j, Tiempo);		
+				}
+			}
+
+			//printf("tiempo %d \n", Tiempo);
+			if (Tiempo == pasos-1) {
+				Tiempo = 0;		
+			}
+			else {
+				Tiempo = Tiempo + 1;
+			}
+			
 		}
-		*/
-		DestroyBox = b2PolygonShape();
-		DestroyBox.SetAsBox(30.0f, 30.0f);
-		b2Transform transformDestruction;
-		b2Vec2 posicionDestroy = b2Vec2(50.0f, -10.0f);
-		transformDestruction.Set(posicionDestroy, 0);
-		//printf("entro\n");
-		b2Vec2 pos(20.0f, 150.0f);
-		for (int i = 0; i < NUM_PARTICLES; ++i) {
-			b2Vec2* posicionesParticulas = m_particleSystems[i]->GetPositionBuffer();
-			for (int j = 0; j < 1; j++) {
-				//printf("Posicion %f %f\n", posicionesParticulas[j].x, posicionesParticulas[j].y);
-				if (DestroyBox.TestPoint(transformDestruction, posicionesParticulas[j]))
-				{
-					m_particleSystems[i]->DestroyParticle(j);
-					//printf("destruyo\n");
+	}
+
+	void amplitud() {
+		// Onda inicial y condiciones de la barrera
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				u_present[i][j] = 0.0;
+				u_past[i][j] = 0.0; //se requiere porque necesitamos el punto anterior para implementar el método
+				// Fuente de onda inicial en el centro de la rejilla
+				if (i == N / 2 && j == N / 2) {
+					u_present[i][j] = 1.0;  // Amplitud inicial
 				}
 			}
 		}
-		//Toda esta parte es la función para destruir las particulas que toquen un box especifico.
-		//por eso se debe hacer en el step que es la función que llama por cada paso.
-		
+
+		// Propagación
+		for (int t = 0; t < pasos; ++t) {  // pasos de tiempo a gusto
+			for (int i = 1; i < N - 1; ++i) {
+				for (int j = 1; j < N - 1; ++j) {
+					
+					// Condición para la barrera vertical con un orificio
+					if (i == barrera_x) {
+						if (j >= orificio_y1 && j <= orificio_y2) {  // Orificio en la barrera
+							// La onda puede pasar por el orificio
+							u_future[i][j] = 2 * u_present[i][j] - u_past[i][j] + c * c * dt * dt * ((u_present[i + 1][j] - 2 * u_present[i][j] + u_present[i - 1][j]) / (dx * dx) +
+								(u_present[i][j + 1] - 2 * u_present[i][j] + u_present[i][j - 1]) / (dy * dy));
+						}
+						else {  // Resto de la barrera
+							u_future[i][j] = barrera_amplitud;  // Onda detenida
+						}
+					}
+					else {  // Puntos fuera de la barrera
+						u_future[i][j] = 2 * u_present[i][j] - u_past[i][j] + c * c * dt * dt * ((u_present[i + 1][j] - 2 * u_present[i][j] + u_present[i - 1][j]) / (dx * dx) +
+							(u_present[i][j + 1] - 2 * u_present[i][j] + u_present[i][j - 1]) / (dy * dy));
+					}
+					Resultados[i][j][t] = u_future[i][j];
+				}
+				printf("\n");
+			}
+
+			// Actualización para el siguiente paso de tiempo
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					u_past[i][j] = u_present[i][j];
+					u_present[i][j] = u_future[i][j];
+				}
+			}
+		}
 	}
 
-	void ColorVel(b2ParticleColor Colors,b2Vec2 velocidad) {
-		//función para cambiar el color de las particulas segun la velovidad.
-		double velx = velocidad.x;
-		double vecy = velocidad.y;
-		Colors.Set(velx, vecy, 2* vecy, 2/ velx);
+	b2ParticleColor ColorAmp(int i, int j,int pasos) {
+		//función para cambiar el color de las particulas segun la amplitud del metodo de diferencias finitas.
+		double amplitud = pow(Resultados[i][j][pasos],1.0/10.0)*255.0;
+		b2ParticleColor ColorFinal(amplitud, amplitud, amplitud, 255);
+		return ColorFinal;
 	}
+
 	static Test* Create()
 	{
 		return new ProyectoCasoUno;
